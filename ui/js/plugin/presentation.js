fb4d5d3e143585d9264243b8bf159fddd58e88c9
Tiery <tierymail@gmail.com>
2013-03-26 11:40:40 +0100

    Syntax changes for linter

--- ui/js/plugin/presentation.js
+++ ui/js/plugin/presentation.js
@@ -1,3 +1,4 @@
+/*global PX:false, jQuery:false */
 (function ($) {
 
     'use strict';
@@ -7,55 +8,56 @@
      */
     var pluginName = 'presentation',
         defaults = {},
+        isTouch = PX.test('touch'),
         Plugin;
-        
+
     /*
      * Plugin constructor
      */
     Plugin = function (element, options, i) {
-        
+
         // Plugin exposed to window
         /* window[pluginName + i] = this; */
-        
+
         // Main element
         this.$element = $(element).attr('data-' + pluginName + '-id', pluginName + i).addClass(pluginName.toLowerCase() + '-init');
-        
+
         // Options
         var opts  = $.extend({}, options, this.$element.data(pluginName + '-options')); // element options -> plugin init options
         this.opts = $.extend({}, defaults, opts); // options -> default
-        
+
         // DOM elements
         /* this.$items    = this.$element.find('li'); */
-                
+
         // Properties
         /* this.status   = 'wait'; // wait | busy */
-        
+
         // Go!
-        this.init();        
+        this.init();
     };
-    
+
     /*
      * Shortcut for Plugin object prototype
      */
     Plugin.fn = Plugin.prototype;
-    
+
     /*
      * Initialization logic
      */
     Plugin.fn.init = function () {
         var that = this;
-        
+
         // Events init
         that.events();
     };
-    
+
     /*
      * Evenements
      */
     Plugin.fn.events = function () {
         var that = this;
     };
-    
+
     // Plugin wrapper around the constructor,
     // preventing against multiple instantiations
     $.fn[pluginName] = function (options) {
